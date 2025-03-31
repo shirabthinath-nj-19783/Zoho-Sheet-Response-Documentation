55ef88570714eb7fccbaf21feab5aa77d6c656eb
Shirabthinath N J <shirabthinath.nj@zohocorp.com>
2025-03-31 12:11:46 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,36 +1,27 @@
 // @ts-check
 // `@type` JSDoc annotations allow editor autocompletion and type checking
 // (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Zoho Sheet',
+  title: 'Zoho Sheet Response Documentation',
   tagline: 'Response Structure',
   favicon: 'https://static.zohocdn.com/sheet/cdn_static/images/favicon.fd1a0ec9def8f9d6d9cb71c842ceb8da.ico',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // Set the production URL of your site
+  url: 'https://shirabthinath-nj-19783.github.io',  // Your GitHub Pages URL
+  baseUrl: '/Zoho-Sheet-Response-Documentation/',  // Your GitHub repo name
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'shirabthinath-nj-19783',  // Your GitHub username
+  projectName: 'Zoho-Sheet-Response-Documentation',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -43,10 +34,7 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/shirabthinath-nj-19783/Zoho-Sheet-Response-Documentation/edit/main/',
         },
         blog: {
           showReadingTime: true,
@@ -54,14 +42,7 @@ const config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          editUrl: 'https://github.com/shirabthinath-nj-19783/Zoho-Sheet-Response-Documentation/edit/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -70,92 +51,53 @@ const config = {
     ],
   ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'Response Structure',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'https://static.zohocdn.com/sheet/cdn_static/images/sheet_logo.7ada89f54b25c6077d79a47f014ad5a9.svg',
+  themeConfig: {
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Response Structure',
+      logo: {
+        alt: 'Zoho Sheet Logo',
+        src: 'https://static.zohocdn.com/sheet/cdn_static/images/sheet_logo.7ada89f54b25c6077d79a47f014ad5a9.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Introduction',
         },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Introduction',
-          },
-          // {
-          //   href: 'https://github.com/facebook/docusaurus',
-          //   label: 'GitHub',
-          //   position: 'right',
-          // },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [{ label: 'Tutorial', to: '/docs/intro' }],
+        },
+        {
+          title: 'Community',
+          items: [
+            { label: 'Stack Overflow', href: 'https://stackoverflow.com/questions/tagged/docusaurus' },
+            { label: 'Discord', href: 'https://discordapp.com/invite/docusaurus' },
+            { label: 'X', href: 'https://x.com/docusaurus' },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            { label: 'Blog', to: '/blog' },
+            { label: 'GitHub', href: 'https://github.com/shirabthinath-nj-19783/Zoho-Sheet-Response-Documentation' },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Zoho Sheet. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  },
 };
 
-module.exports = {
-  // Your existing config...
-  title: 'Zoho Sheet Response Documentation',
-  url: 'https://shirabthinath-nj-19783.github.io',  // Replace with your GitHub username
-  baseUrl: '/Zoho-Sheet-Response-Documentation/',  // Replace with your GitHub repo name
-  projectName: 'Zoho-Sheet-Response-Documentation',
-  organizationName: '',
-  deploymentBranch: 'gh-pages',
-  trailingSlash: false,
-};
-
-export default config;
+module.exports = config;

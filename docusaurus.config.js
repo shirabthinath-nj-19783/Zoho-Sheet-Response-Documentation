59488299017790cc6c955c8d82a9f21f34143701
Shirabthinath N J <shirabthinath.nj@zohocorp.com>
2025-03-31 15:26:38 +0530

    Footers removed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,31 +68,6 @@ const config = {
         },
       ],
     },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [{ label: 'Tutorial', to: '/docs/intro' }],
-        },
-        {
-          title: 'Community',
-          items: [
-            { label: 'Stack Overflow', href: 'https://stackoverflow.com/questions/tagged/docusaurus' },
-            { label: 'Discord', href: 'https://discordapp.com/invite/docusaurus' },
-            { label: 'X', href: 'https://x.com/docusaurus' },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            { label: 'Blog', to: '/blog' },
-            { label: 'GitHub', href: 'https://github.com/shirabthinath-nj-19783/Zoho-Sheet-Response-Documentation' },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Zoho Sheet. Built with Docusaurus.`,
-    },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
